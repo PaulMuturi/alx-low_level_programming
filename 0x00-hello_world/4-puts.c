0f1af7d5431ac7576549f2b2b506010238ed97c3
PaulMuturi <muturipaul10@gmail.com>
2023-02-16 21:32:35 +0000

    betty fix_1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+*main-Entry point of the program
+*
+*Return: exit status of the program
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
