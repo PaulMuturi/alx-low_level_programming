5ea9efd57cd9dcf532be23641f20a797bf277cee
PaulMuturi <muturipaul10@gmail.com>
2023-02-16 23:09:37 +0000

    first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Program entry point
+*
+*Return: exit status of the program
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
