847b28c8446738fba29df1d8e51e5a185b9eb701
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 18:30:09 +0000

    fix 5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	int first_digit, second_digit, third_digit;
 	int digit = 1;
+	int add_comma = 0;
 
 	while (digit < 1000)
 	{
@@ -17,16 +18,19 @@ int main(void)
 		second_digit = second_digit / 10;
 		third_digit = digit % 10;
 
+
 		if (third_digit > second_digit && second_digit > first_digit)
 	{
-			if (digit > 10)
+			if (add_comma)
 			{
 				putchar(',');
 				putchar(' ');
+
 			}
 			putchar(first_digit + '0');
 			putchar(second_digit + '0');
 			putchar(third_digit + '0');
+			add_comma = 1;
 		}
 		digit++;
 	}
