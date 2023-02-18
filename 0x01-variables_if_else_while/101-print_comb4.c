9ee754ddf8913db925fbf785fb2e44d484907d19
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 18:06:00 +0000

    first commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+*main - Entry point of the program.
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	int first_digit, second_digit, third_digit;
+	int digit = 1;
+
+	while (digit < 1000)
+	{
+		first_digit = digit / 100;
+		second_digit = digit / 10;
+		third_digit = digit % 10;
+
+		if (third_digit > second_digit && second_digit > first_digit)
+		{
+			if (digit > 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+			putchar(third_digit + '0');
+		}
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
