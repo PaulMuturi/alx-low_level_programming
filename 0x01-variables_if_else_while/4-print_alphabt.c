8e970764007859c0de3a3906a6e707fc422f656f
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 10:45:32 +0000

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - Entry poin of th program"
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	char current_char = 'a';
+
+	while (current_char <= 'z')
+	{
+		if (current_char != 'q' && current_char != 'e')
+		{
+			putchar(current_char);
+			current_char++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
