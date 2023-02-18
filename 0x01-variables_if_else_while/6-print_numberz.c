ecb0d5cd86b3b05a170fe259dbf187c8e9f802a6
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 11:35:27 +0000

    first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Entry poin of th program"
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	char current_int = 0;
+
+	while (current_int < 10)
+	{
+		putchar(current_int);
+		current_int++;
+	}
+
+	putchar('\n');
+	return (0);
+}
