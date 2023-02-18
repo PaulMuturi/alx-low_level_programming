9b577476141511a8dacd3ca04e78c321a25b4952
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 17:00:36 +0000

    first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Entry poitn of the program"
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	int current_int = 0;
+
+	while (current_int < 10)
+	{
+		putchar(current_int + 0);
+		putchar(',');
+		putchar(' ');
+		current_char++;
+	}
+
+	putchar('\n');
+	return (0);
+}
