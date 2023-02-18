efee2028b9adc319ed7cc082ce80df194706f3ce
PaulMuturi <muturipaul10@gmail.com>
2023-02-18 14:48:25 +0000

    first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - Entry point of the program"
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	char current_int = 0;
+
+	while (current_int < 10)
+	{
+		printf("%d", current_int);
+		current_int++;
+	}
+
+	hex_letter = 'A';
+
+	while (hex_letter <= 'F')
+	{
+		putchar(hex_letter);
+	}
+	putchar('\n');
+	return (0);
+}
