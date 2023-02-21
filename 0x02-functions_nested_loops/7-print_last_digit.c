6d376a3a3d075d1aefcdd3a7c89c1f7d1e89f09c
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 21:50:21 +0000

    first commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *print_last_digit - prints last digit of a number
+  *@n: input integer number
+  *
+  *Return: last integer digit of the input number
+ */
+int print_last_digit(int n)
+{
+	return ((n % 10));
+}
