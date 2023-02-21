efb3ea50e6bd8c1e84f4a64e092f1f9d9eafa4c8
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 23:07:46 +0000

    Introduce abs_long function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+long int abs_long(int);
 
 /**
   *print_last_digit - prints last digit of a number
@@ -11,10 +12,26 @@ int print_last_digit(int n)
 {
 	int lastdigit;
 
-	int long m = (long)n;
-	lastdigit = abs(m) % 10;
+	lastdigit = abs_long(n) % 10;
 
 	_putchar(lastdigit + '0');
 	return (lastdigit);
 }
 
+/**
+ *abs_long - Takes in int and outputs its sbsolute value in long format
+ *This is to handle INT_MIN overflow issues
+ *@j: int to compute
+ *
+ *Return: computed abs in long format
+*/
+long int abs_long(int j)
+{
+	int long m = (long)n;
+	if (m < 0)
+	{
+		return (m * long(-1));
+	}
+
+	return (m);
+}
