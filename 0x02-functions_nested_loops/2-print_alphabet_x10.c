7ebb2e3eceba77e83ccf99317310c1a72f553116
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 20:09:19 +0000

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/**print_alphabet_x10 - prints alpahet in lowercase 10 times
+  *
+  *Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alphaletter;
+
+	for (i = 0; i < 10; i++)
+	{
+		alphaletter = 'a';
+
+		while (alphaletter <= 'z')
+		{
+			_putchar(alphaletter);
+			alphaletter++;
+		}
+		_putchar('\n');
+	}
+}
+
+
