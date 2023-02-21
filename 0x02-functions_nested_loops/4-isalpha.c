d9254c3fe00bd6d02ed0fc206e57722c5e3bef3e
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 21:02:20 +0000

    first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *_isalpha - checks for alphabetic character
+  *@c: character to be evaluated
+  *
+  *Return: value 1 if letter is alphabetic and 0 if otherwise
+*/
+int _isalpha(int c)
+{
+	char lowerletter = 'a';
+	char upperletter = 'A';
+
+	while (lowerletter <= 'z')
+	{
+		if (c == lowerletter)
+			return (1);
+		lowerletter++
+	}
+
+	while (upperletter <= 'Z')
+	{
+		if (c == upperletter)
+			return (1);
+		upperletter++
+	}
+
+	return (0);
+}
