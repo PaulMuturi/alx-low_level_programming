77eb6962bededa4ceb794645bb2a01761c0ae15e
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 21:38:51 +0000

    first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *_abs - computes the absolute value of an integer
+  *@n: value to compute
+  *
+  *Return: the computed integer value
+ */
+int _abs(int n)
+{
+	return (n * (-1));
+}
