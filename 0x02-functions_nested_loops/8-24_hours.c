27fdfc15fc9f9282fbcaf9e3b6cf984ba79a15fb
PaulMuturi <muturipaul10@gmail.com>
2023-02-21 23:33:04 +0000

    first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  *jack_bauer - prints every minute of a day starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		if (i < 10)
+		{
+			_putchar('0');
+		}
+
+		_putchar(i + '0');
+		_putchar(':');
+
+		for (j = 0; j < 60; j++)
+		{
+			if (j < 10)
+			{
+				_putchar('0');
+			}
+
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+}
