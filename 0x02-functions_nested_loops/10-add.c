97d0eddf3d7981fc67a714920635759c1bd6b55d
PaulMuturi <muturipaul10@gmail.com>
2023-02-22 00:40:19 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *add - adds twi integers and returns the result
+  *@a: first integer operand
+  *@b: second integer operand
+  *
+  *Return: addition result of integer type
+ */
+int add(int a, int b)
+{
+	return ((a + b));
+}
