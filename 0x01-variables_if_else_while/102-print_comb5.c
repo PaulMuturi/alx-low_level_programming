c6583c04286e9be09a6b665b5cc6bffcca1d88e4
PaulMuturi <muturipaul10@gmail.com>
2023-02-23 17:22:55 +0000

    add README.md

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+*main - Entry point of the program.
+*
+*Return: Exit status of the program"
+*/
+int main(void)
+{
+	int first_digit, last_digit;
+	int digit = 0;
+
+	while (digit < 100)
+	{
+		first_digit = digit / 10;
+		last_digit = digit % 10;
+
+		if (last_digit > first_digit)
+		{
+			if (digit > 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(first_digit + '0');
+			putchar(last_digit + '0');
+		}
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
