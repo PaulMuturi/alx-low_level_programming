55ad8aed14de9417a43d3bf43d94cc5091204ff1
PaulMuturi <muturipaul10@gmail.com>
2023-02-23 23:26:48 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *main - entry point to the program
+  *Return: exit status of the program
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+}
