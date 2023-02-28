9defbefb0ef608e9d8adf26dc0f912074143d0a2
PaulMuturi <muturipaul10@gmail.com>
2023-02-28 01:34:06 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *_strlen - returns the length of a string
+  *@s:string
+  *
+  *Return: integer , string length
+  */
+
+int _strlen(char *s)
+{
+	int strlength = strlen(s);
+
+	return (strlength);
+}
