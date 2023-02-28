93d765f43a9525017201f8d759fd8b20381174d5
PaulMuturi <muturipaul10@gmail.com>
2023-02-28 03:49:55 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  *rev_string - reverses a string"
+  *@s: string
+  *
+  *Return: void
+  */
+
+void rev_string(char *s)
+{
+	int strlength = strlen(s);
+	int i;
+	char str[1000];
+
+	strcpy(str, s);
+
+	for (i = 0; i < strlength; i++)
+	{
+		s[i] = str[((strlength - 1) - i)];
+	}
+
+
+}
