be138356a0cab818a5eb0b4b2b29d25b421298f8
PaulMuturi <muturipaul10@gmail.com>
2023-03-02 09:23:52 +0000

    first commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,8 +2,9 @@
 #include <string.h>
 
 /**
-  *strncpy - copies a string
+  *_strncpy - copies a string
   *@dest: destination string
+  *@src: string to copy
   *@n: integer, copy limit
   *
   *Return: pointer to dest
@@ -11,6 +12,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+	int srclen = strlen(src);
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	if (srclen < n)
+		dest[i] = '\0';
+
+	return (dest);
 }
 
