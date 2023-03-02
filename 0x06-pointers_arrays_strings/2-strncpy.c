c4e6340c3151d9c62afcd6e14b58b3110817fcb8
PaulMuturi <muturipaul10@gmail.com>
2023-03-02 09:31:04 +0000

    fix 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int srclen = strlen(src);
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	if (srclen < n)
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
