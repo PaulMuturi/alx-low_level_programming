3b2197628b73bcd89d251ba93c437785f5439436
PaulMuturi <muturipaul10@gmail.com>
2023-03-02 09:42:13 +0000

    1-strncat restored

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[destlen + i] = src[i];
 
-	for (; i < n; i++)
+	if  (i < n)
 		dest[destlen + i] = '\0';
 
 	return (dest);
