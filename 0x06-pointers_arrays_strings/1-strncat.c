48180d06f5cc912f8331d88213371c188db84663
PaulMuturi <muturipaul10@gmail.com>
2023-03-02 09:03:47 +0000

    fix 5

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	int destlen = strlen(dest);
 	int srclen = strlen(src);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[destlen + i] = src[i];
 	}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *strncpy - copies a string
+  *@dest: destination string
+  *@n: integer, copy limit
+  *
+  *Return: pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
+
