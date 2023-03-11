f0c6342638578f7fab166c0c7d48dfd919958b75
PaulMuturi <muturipaul10@gmail.com>
2023-03-11 17:26:47 +0000

    add helper check functions

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 int _pow(int, int);
+int _isoperator(char);
+char _opeval(char, char);
+int _isdigit(char);
 /**
   *_atoi - converts a sring to integer
   *@s: string
@@ -11,26 +14,49 @@ int _pow(int, int);
 int _atoi(char *s)
 {
 	int i;
-	int isneg = 0;
+	char oper = ' ';
 	int multiplier = 0;
 	int sum = 0;
 	char *firstdgt = NULL;
+	char prev = ' ';
+	char cur = ' ';
+	int isdgt;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{	
+		isdgt = _isdigit(s[i]);
+
+		if (_isoperator(s[i]))
+		{
+			cur = s[i];
+			if (i > 0 && _isoperator(s[i - 1]))
+				prev = s[i - 1];
+			else
+				prev = ' ';
+
+			if (prev != ' ' && oper != ' ')
+				prev = oper;
+
+			oper = _opeval(prev, cur);
+		}
+		else if (!isdgt)
+		{
+			cur = ' ';
+			prev = ' ';
+			oper = ' ';
+		}
+
+		if (isdgt)
+		{
 			if (!firstdgt)
 			{
 				firstdgt = &(s[i]);
 
-				if (s[i - 1] == '-')
-					isneg = 1;
 			}
 
 			multiplier++;
 		}
-		else if(firstdgt)
+		else if (firstdgt)
 			break;
 	}
 
@@ -42,7 +68,7 @@ int _atoi(char *s)
 			firstdgt++;
 		}
 
-		if (isneg)
+		if (oper == '-')
 			sum *= -1;
 	}
 
@@ -61,7 +87,7 @@ int _pow(int n, int p)
 {
 	int i = 1;
 	int prod = 1;
-	
+
 	while (i < p)
 	{
 		prod *= n;
@@ -70,3 +96,50 @@ int _pow(int n, int p)
 
 	return (prod);
 }
+
+/**
+  *_isoperator - checks if character is - or +
+  *@c: character
+  *
+  *Return: 1 if true, otherwise 0
+  */
+int _isoperator(char c)
+{
+	if (c == '+' || c == '-')
+		return (1);
+
+	return (0);
+}
+
+/**
+  *_opeval - does arithmetic of two operands following each other
+  *@prev: operator 1
+  *@cur: operator 2
+  *
+  *Return: resulting operator
+  */
+char _opeval(char prev, char cur)
+{
+	if ((prev == '-' && cur == '-') || (prev == '+' && cur == '+'))
+		return ('+');
+
+	if ((prev == '-' && cur == '+') || (prev == '+' && cur == '-'))
+		return ('-');
+
+	return (cur);
+}
+
+/**
+  *_isdigit - checks if character is digit
+  *@c: character
+  *
+  *Return: 1 if true,otherwise 0
+  */
+int _isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
