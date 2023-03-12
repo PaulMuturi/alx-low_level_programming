4740f025b4231cf0dca24de4ea854026d6b1cd7e
PaulMuturi <muturipaul10@gmail.com>
2023-03-12 00:56:44 +0000

    fix 1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+int checksym(char *);
 /**
   *main - adds positive numbers
   *@argc: arguments count
@@ -11,7 +12,10 @@
 
 int main(int argc, char **argv)
 {
-	int i, num, sum;
+	int i;
+	int sum = 0;
+	char *num;
+	int no_sym;
 
 	if (argc == 1)
 	{
@@ -19,23 +23,42 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-
 		for (i = 1; i < argc; i++)
 		{
-			num = atoi(argv[i]);
+			num = argv[i];
+			no_sym = checksym(num);
 
-			if (!num)
+			if (no_sym)
+				sum += atoi(num);
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-
-			if (num > 0)
-				sum += num;
 		}
 
 		printf("%d\n", sum);
 	}
 
 	return (0);
+}
+
+/**
+  *checksym - check for any non digit symbol
+  *@n: number string
+  *
+  *Return: 1 if no symbol, 0 if symbol found
+  */
+int checksym(char *n)
+{
+	int j;
+
+	for (j = 0; n[j] != '\0'; j++)
+	{
+		if (!(n[j] >= '0' && n[j] <= '9'))
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
