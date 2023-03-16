c63b66903e85603be1e2ec3b9b19fb3ce0cad12c
PaulMuturi <muturipaul10@gmail.com>
2023-03-16 18:20:15 +0000

    return ptr if new and old are same size

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *ptr2;
 	unsigned int i;
 
-
 	if (ptr && new_size == 0)
 	{
 		free(ptr);
@@ -20,7 +19,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (new_size == old_size)
-		return (NULL);
+		return (ptr);
+
 	ptr2 = malloc(new_size);
 
 	if (!ptr2)
