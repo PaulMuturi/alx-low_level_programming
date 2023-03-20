21c46119cffbae586a6f55f5aa1faabfbece6f99
PaulMuturi <muturipaul10@gmail.com>
2023-03-20 14:03:08 -0700

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - new type
+*@name: name of dog, string
+*@age: age of dog, float
+*@owner: name of dog owner, string
+*
+*Description: Gives a brief on dog structure
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
