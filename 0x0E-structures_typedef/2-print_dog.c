3e2203b3e9f3aed37ac9a5b438d9e35a9f17b056
PaulMuturi <muturipaul10@gmail.com>
2023-03-21 12:38:31 +0000

    remove paranthesis in nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,14 +13,14 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+			printf("Name: nil\n");
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("(nil)");
+			printf("nil");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("(nil)\n");
+			printf("nil\n");
 	}
 }
