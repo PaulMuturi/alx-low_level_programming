71dd50bfa30104da40bf04f422e7913607ec1703
PaulMuturi <muturipaul10@gmail.com>
2023-03-23 16:07:59 +0000

    check for more than 1 char operand

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,8 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (*(ops[i].op))
+	while (*(ops[i].f))
 	{
+		if (strlen(s) > 1)
+			break;
+
 		if (*(ops[i].op) == *s)
 			return (*(ops[i].f));
 		i++;
