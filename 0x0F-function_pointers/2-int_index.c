5f305a9a9667fd60d79efe72ed4c46f95f07d13f
PaulMuturi <muturipaul10@gmail.com>
2023-03-23 10:03:32 +0000

    first commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+/**
+  *int_index - searches for an integer
+  *@array: array of int to be searched
+  *@size: array size
+  *@cmp: pointer to function used to compare values
+  *
+  *Return: index of the first element found, else -1
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int res, i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			res = cmp(array[i]);
+
+			if (res)
+			{
+				return (i);
+			}
+		}
+	}
+
+	return (-1);
+}
