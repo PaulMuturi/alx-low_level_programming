3495736b48017156664cbaca6821989d0d32f90d
PaulMuturi <muturipaul10@gmail.com>
2023-03-31 03:04:01 +0000

    add file 4-free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  *free_list - frees a give list_t"
+  *@head: list to be freed
+  *
+  *Return: void
+  */
+void free_list(list_t *head)
+{
+	list_t *nxt, *newnxt;
+	if (head)
+	{
+		nxt = head;
+		while (nxt->next != NULL)
+		{
+			newnxt = nxt->next;
+			free(nxt);
+			nxt = newnxt;
+		}
+		free(nxt);
+	}
+}
