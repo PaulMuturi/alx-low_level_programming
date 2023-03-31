3364b435186bccb90c166ca6498e5aba5f447372
PaulMuturi <muturipaul10@gmail.com>
2023-03-31 04:00:31 +0000

    freed all memory, including str from strdup in 4-free_list.c file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,15 +8,19 @@
 void free_list(list_t *head)
 {
 	list_t *nxt, *newnxt;
+
 	if (head)
 	{
 		nxt = head;
 		while (nxt->next != NULL)
 		{
 			newnxt = nxt->next;
+			free(nxt->str);
 			free(nxt);
 			nxt = newnxt;
 		}
+
+		free(nxt->str);
 		free(nxt);
 	}
 }
