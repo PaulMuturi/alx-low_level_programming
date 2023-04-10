6c0f3cf3cccc2c8e821e42ead251359e0d8185cc
PaulMuturi <muturipaul10@gmail.com>
2023-04-10 19:33:20 +0000

    handle free memory in 0-read_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,21 +24,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 
 	if (fd < 0)
+	{
+		free(ch);
 		return (0);
+	}
 
 	bt_rd = read(fd, ch, letters);
 	if (bt_rd <= 0)
+	{
+		free(ch);
 		return (0);
+	}
 
 	for (i = 0; i < bt_rd / sizeof(char); i++)
 	{
 		bt_wr = write(1, ch + i, sizeof(char));
 
 		if (bt_wr <= 0)
+		{
+			free(ch);
 			return (0);
+		}
 
 		count++;
 	}
 
+	free(ch);
 	return (count);
 }
