179c151a79fc18aa6a2db320f196c6183dc2c525
PaulMuturi <muturipaul10@gmail.com>
2023-04-10 20:41:19 +0000

    attempt fix 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *ch;
-	int fd;
+	int fd, to_loop = 1;
 	size_t i;
 	ssize_t bt_rd, bt_wr, count = 0;
 
@@ -23,27 +23,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-	{
-		free(ch);
-		return (0);
-	}
+		to_loop = 0;
 
 	bt_rd = read(fd, ch, letters);
 	if (bt_rd <= 0)
-	{
-		free(ch);
-		return (0);
-	}
-	for (i = 0; i < bt_rd / sizeof(char); i++)
+		to_loop = 0;
+
+	for (i = 0; i < bt_rd / sizeof(char) && to_loop; i++)
 	{
 		bt_wr = write(1, ch + i, sizeof(char));
 		if (bt_wr <= 0 || bt_wr != sizeof(char))
-		{
-			free(ch);
-			return (0);
-		}
+			to_loop = 0;
+
 		count++;
 	}
+	if (!to_loop)
+	{
+		free(ch);
+		close (fd);
+		return (0);
+	}
+
 	free(ch);
 	return (count);
 }
