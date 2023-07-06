67367fa71264901d4a3aacb2bac999dd98e5cc6d
PaulMuturi <muturipaul10@gmail.com>
2023-07-06 13:05:48 +0000

    add 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+  *binary_to_uint - converts a binary number to a number
+  *@b: pointer to a string
+  *Return: converted number
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	int len, i;
+	char c;
+
+	unsigned int sum = 0, factor = 1;
+
+	if (!b)
+		return (0);
+
+	len = _strlen(b);
+
+	for (i = (len - 1); i >= 0; i--)
+	{
+		c = b[i];
+		if (c != '1' || c != '0')
+			return (0);
+
+		sum += (factor * (c - '0'));
+		factor *= 2;
+	}
+	return (sum);
+}
+
+/**
+  *_strlen - counts number of characters in a string
+  *@b: pointer to string
+  *Return: number of chars in string
+  */
+int _strlen(const char *b)
+{
+	int i = 0;
+	int len = 0;
+
+	while (b[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
