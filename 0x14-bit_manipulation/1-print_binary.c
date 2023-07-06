f4e485201f10f04779e73b3dfe259378f46b2e3f
PaulMuturi <muturipaul10@gmail.com>
2023-07-06 14:42:24 +0000

    1-print_binary.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,14 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int count = sizeof(n) * 8, m = 1, a, tmp, i;
 	int isbin = 0;
-	
+
 	for (i = 0; i < count; i++)
 	{
 		tmp = n >> ((count - 1) - i);
 		a = tmp & m;
 		if (a)
 			isbin = 1;
-		
+
 		if (isbin)
 			_putchar(a + '0');
 	}
