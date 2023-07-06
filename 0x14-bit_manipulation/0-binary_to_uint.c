d6181c617500e673a1ac11d4fb4aec6a67606d3b
PaulMuturi <muturipaul10@gmail.com>
2023-07-06 14:24:58 +0000

    0-binary_to_uint fixt tabs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
-  *binary_to_uint - converts a binary number to an unsigned int
+ *binary_to_uint - converts a binary number to an unsigned int
   *@b: string of 0 and 1 chars
   *
   *Return: converted unsigned integer,
   * Returns 0 if b is not 0 or 1 or is NULL
-  */
+ */
 unsigned int binary_to_uint(const char *b)
 {
-        int i, count = 0, sum = 0, exp = 0;
-        char c;
+	int i, count = 0, sum = 0, exp = 0;
+	char c;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (i = 0; b[i] != '\0'; i++)
-        {
-                c = b[i];
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		c = b[i];
 
-                if (c != '0' && c != '1')
-                        return (0);
+		if (c != '0' && c != '1')
+			return (0);
 
-                count++;
-        }
+		count++;
+	}
 
-        for (i = (count - 1); i >= 0; i--)
-        {
-                if (exp == 0)
-                        exp = 1;
-                else
-                        exp *= 2;
+	for (i = (count - 1); i >= 0; i--)
+	{
+		if (exp == 0)
+			exp = 1;
+		else
+			exp *= 2;
 
-                sum += (b[i] - '0') * exp;
-        }
+		sum += (b[i] - '0') * exp;
+	}
 
-        return ((unsigned int)sum);
+	return ((unsigned int)sum);
 }
